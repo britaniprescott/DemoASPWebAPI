a315ae724c1b367d39fe172d5528eed79de0effe
Britani Prescott <britaniprescott@gmail.com>
2019-11-03 01:37:52 +0000

    Added [HttpGet({id})]

--- src/ContosoPets.Api/Controllers/ProductsController.cs
+++ src/ContosoPets.Api/Controllers/ProductsController.cs
@@ -23,7 +23,18 @@ namespace ContosoPets.Api.Controllers
         public ActionResult<List<Product>> GetAll() =>
             _context.Products.ToList();
 
-        // GET by ID action
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(long id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
 
         // POST action
 
